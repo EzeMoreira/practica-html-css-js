1c42453a9e4632865e34703723cb2732f93bfa61
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-15 11:39:42 -0300

    changes in funcitons

--- codigo.js
+++ codigo.js
@@ -1,5 +1,7 @@
 const boton = document.getElementById('compro');
 const botonFav = document.getElementById('favorito');
+const botonNum2 = document.getElementById('comproNum2');
+const botonFav2 = document.getElementById('favoritoFav2');
 
 function mostrarCartel(){
     boton.innerHTML = "Producto comprado con éxito"
