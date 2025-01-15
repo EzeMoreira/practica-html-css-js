db730a75a9abb42ffec8923f1379c30121bba3e8
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-15 11:32:09 -0300

    1° modificacion HTML

--- codigo.js
+++ codigo.js
@@ -1,5 +1,10 @@
 const boton = document.getElementById('compro');
+const botonFav = document.getElementById('favorito');
 
 function mostrarCartel(){
     boton.innerHTML = "Producto comprado con éxito"
+}
+
+function mostrarCartelFav(){
+    boton.innerHTML = "Producto marcado como favorito"
 }
